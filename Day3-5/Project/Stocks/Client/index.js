db24164f0adcd5aac990421966ba96a8d361d428
Amilcov <Amilcov@yahoo.com>
2022-09-03 18:37:44 +0300

    Add files via upload

--- Day3-5/Project/Stocks/Client/index.js
+++ Day3-5/Project/Stocks/Client/index.js
@@ -5,26 +5,52 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/register', (req, res) => {
+    return res.render('signup', {title: "Register"});
+});
+
+app.use('/stock/add', (req, res) => {
+    return res.render('stock-add', {title: "Add Stock"});
+});
 
 
-app.use('/login', (req, res) => {
+app.use('/stock/:id(\\d+)', (req, res) => {
+    const stockId = parseInt(req.params.id, 10);
+    return res.render('stock-detail', {title: "Stock Detail", stockId: stockId});
+});
+
+app.use('/stock/edit/:id(\\d+)', (req, res) => {
+    const stockId = parseInt(req.params.id, 10);
+    return res.render('stock-edit', {title: "Stock Edit", stockId: stockId});
+});
+
+app.use('/stock/delete/:id(\\d+)', (req, res) => {
+    const stockId = parseInt(req.params.id, 10);
+    return res.render('stock-delete', {title: "Stock Delete", stockId: stockId});
+});
+
+
+app.use('/stock', (req, res) => {
+    return res.render('stock-list', {title: "Stocks"});
+});
+
+
+app.use('/transactions', (req, res) => {
+    return res.render('transaction-list', {title: "Transactions"});
+});
+app.use('/transaction/add', (req, res) => {
+    return res.render('transaction-add', {title: "Add Transactions"});
+});
+
+app.use('/login',(req, res) => {
     return res.render('login', {"title": "Login"});
 });
 
-app.use('/register', (req, res) => {
-    return res.render('signup', {title: "Register"})
+app.use('/', (req, res) => { 
+    return res.render('login', {"title": "Login"});
 });
-
-app.use('/stocks', (req, res) => {
-    return res.render('stocks-list', {title: "Stocks", stocks: []})
-});
-
-app.use('/transactions', (req, res) => {
-    return res.render('transactions-list', {title: "Transactions", transactions: []})
-});
-app.use('/home', (req, res) => {
-    return res.json({"home":"home"});
-})
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {console.log(`Server is listening on port ${PORT}...`)});
+app.listen(PORT, () => {console.log(`Server is listening on port ${PORT}, date ${new Date().toLocaleDateString()}...`)});
